4898699d48bfc8eeba9313c62727ed5c7d7fcebc
DESKTOP-FBEOFRH\Valentin <valetordi@gmail.com>
2024-08-29 11:57:23 -0300

    Termine el in-order

--- src/main/java/tp2/Main.java
+++ src/main/java/tp2/Main.java
@@ -1,5 +1,7 @@
 package tp2;
 
+import tp1.ejercicio2.ListaGenerica;
+
 public class Main {
     public static void main(String[] args) {
         var recorredor = new RecorridosAG();
@@ -10,15 +12,32 @@ public class Main {
 
         arbolGeneral.agregarHijo(arbolGeneral2);
         arbolGeneral.agregarHijo(new ArbolGeneral<>(15));
-        //var listaPreOrder = recorredor.recorrerPreOrder(arbolGeneral);
-        //EN PRE-ORDER --> 10 - 11 - 5 - 15
-        //System.out.println("Tamanio de lista: " + listaPreOrder.tamanio()); //Lista recorrida preorder sin filtrar ningun dato
 
+        //Test pre-order --> 10 - 11 - 5 - 15
+        //var listaPreOrder = recorredor.recorrerPreOrder(arbolGeneral);
+        //System.out.println("Tamanio de lista: " + listaPreOrder.tamanio()); //Lista recorrida preorder sin filtrar ningun dato
         //var listaFiltrada = recorredor.numerosImparesMayoresQuePreOrden(arbolGeneral, 13);
         //System.out.println("Tamanio de lista: " + listaFiltrada.tamanio());
 
-        var listaPostOrder = recorredor.recorrerPostOrder(arbolGeneral);
-        //EN post-ORDER --> 11 - 5 - 15 - 10
+        //Test post-order --> 11 - 5 - 15 - 10
+        /*var listaPostOrder = recorredor.recorrerPostOrder(arbolGeneral);
         System.out.println("Tamanio de lista: " + listaPostOrder.tamanio());
+        var listaFiltradaPostOrder = recorredor.numerosImparesMayoresQuePostOrden(arbolGeneral, 13);
+        System.out.println("Tamanio de la lista postorder filtrada: " + listaFiltradaPostOrder.tamanio());
+        listaPostOrder.comenzar();
+        while (!listaPostOrder.fin()) {
+            System.out.println(listaPostOrder.proximo());
+        }*/
+
+        //Probando me di cuenta que
+
+
+        //Test in-order --> 5 - 11 - 10 - 15
+        ListaGenerica<Integer> listaInOrder = recorredor.recorrerInOrder(arbolGeneral);
+        System.out.println("Tamanio de la lista in-order: " + listaInOrder.tamanio());
+        listaInOrder.comenzar();
+        while (!listaInOrder.fin()) {
+            System.out.println(listaInOrder.proximo());
+        }
     }
 }
--- src/main/java/tp2/RecorridosAG.java
+++ src/main/java/tp2/RecorridosAG.java
@@ -13,38 +13,16 @@ public class RecorridosAG {
         return primero > segundo;
     }
 
-    /*
+
     public ListaGenerica<Integer> recorrerPreOrder(ArbolGeneral<Integer> arbolGeneral) {
-        System.out.println("Nuevo arbol...");
+//        System.out.println("Nuevo arbol...");
         ListaGenerica<Integer> listaFinal = new ListaEnlazadaGenerica<Integer>();
-        System.out.println("Entro al if esVacio: " + !arbolGeneral.esVacio());
+//        System.out.println("Entro al if esVacio: " + !arbolGeneral.esVacio());
         if (!arbolGeneral.esVacio()) {                                       //Si el arbol no es vacio entra al if
             listaFinal.agregarFinal(arbolGeneral.getDato());                //Agrego el dato del arbol a la lista final
             System.out.println(arbolGeneral.getDato());
             if (arbolGeneral.tieneHijos()) {
-                System.out.println("Entro tiene hijos: " + arbolGeneral.tieneHijos());
-                ListaGenerica<ArbolGeneral<Integer>> listaHijos = arbolGeneral.getHijos();//Guardo los hijos del arbol en otra estructura auxiliar
-                listaHijos.comenzar();                                                //Preparo la lista
-                while (!listaHijos.fin()) { //Miestras la lista de hijos no llegue a su fin. fin() da true si llegamos al ultimo elemento de la lista
-                    ArbolGeneral<Integer> hijoActual = listaHijos.proximo();    //Retorna el elemento actual y avanza al siguiente.
-                    ListaGenerica<Integer> listaHijosAux = this.recorrerPreOrder(hijoActual);
-                    while (!listaHijosAux.fin()) {  //Mientras no llegue al final
-                        listaFinal.agregarFinal(listaHijosAux.proximo());
-                    }
-                }
-            }
-        }
-        return listaFinal;
-    }*/
-    public ListaGenerica<Integer> recorrerPreOrder(ArbolGeneral<Integer> arbolGeneral) {
-        System.out.println("Nuevo arbol...");
-        ListaGenerica<Integer> listaFinal = new ListaEnlazadaGenerica<Integer>();
-        System.out.println("Entro al if esVacio: " + !arbolGeneral.esVacio());
-        if (!arbolGeneral.esVacio()) {                                       //Si el arbol no es vacio entra al if
-            listaFinal.agregarFinal(arbolGeneral.getDato());                //Agrego el dato del arbol a la lista final
-            System.out.println(arbolGeneral.getDato());
-            if (arbolGeneral.tieneHijos()) {
-                System.out.println("Entro tiene hijos: " + arbolGeneral.tieneHijos());
+//                System.out.println("Entro tiene hijos: " + arbolGeneral.tieneHijos());
                 ListaGenerica<ArbolGeneral<Integer>> listaHijos = arbolGeneral.getHijos();//Guardo los hijos del arbol en otra estructura auxiliar
                 listaHijos.comenzar();                                                //Preparo la lista
                 while (!listaHijos.fin()) { //Miestras la lista de hijos no llegue a su fin. fin() da true si llegamos al ultimo elemento de la lista
@@ -59,8 +37,8 @@ public class RecorridosAG {
         return listaFinal;
     }
 
-    public ListaGenerica<Integer> recorrerPostOrder(ArbolGeneral<Integer> arbolGeneral) {
-        System.out.println("Nuevo arbol...");
+    public ListaGenerica<Integer> recorrerPostOrder(ArbolGeneral<Integer> arbolGeneral) {//salida --> 10 - 11 - 5 - 15
+//        System.out.println("Nuevo arbol...");
         ListaGenerica<Integer> listaFinal = new ListaEnlazadaGenerica<Integer>();
         if (!arbolGeneral.esVacio()) {                                       //Si el arbol no es vacio entra al if
             if (arbolGeneral.tieneHijos()) {
@@ -76,6 +54,62 @@ public class RecorridosAG {
             }
             listaFinal.agregarFinal(arbolGeneral.getDato());                //Agrego el dato del arbol a la lista final
             System.out.println(arbolGeneral.getDato());
+        }
+        return listaFinal;
+    }
+
+    public ListaGenerica<Integer> recorrerPostOrder2(ArbolGeneral<Integer> arbolGeneral) { //salida --> 5-11-15-10
+        System.out.println("Entro al arbol con dato (Sin guardarlo, solo entro): " + arbolGeneral.getDato());
+        ListaGenerica<Integer> listaFinal = new ListaEnlazadaGenerica<Integer>();
+        if (!arbolGeneral.esVacio()) {                                       //Si el arbol no es vacio entra al if
+            if (!arbolGeneral.tieneHijos()) {//Si el arbol no tiene hijos, entra al if
+            } else {
+                ListaGenerica<ArbolGeneral<Integer>> listaHijos = arbolGeneral.getHijos();//Guardo los hijos del arbol en otra estructura auxiliar
+                listaHijos.comenzar();                                                     //Preparo la lista
+                while (!listaHijos.fin()) { //Miestras la lista de hijos no llegue a su fin. fin() da true si llegamos al ultimo elemento de la lista
+                    ArbolGeneral<Integer> hijoActual = listaHijos.proximo();    //Retorna el elemento actual y avanza al siguiente.
+                    ListaGenerica<Integer> listaNumerosResultanteDeLaRecursion = this.recorrerInOrder(hijoActual);
+//                    listaFinal = this.recorrerInOrder(hijoActual);
+                    while (!listaNumerosResultanteDeLaRecursion.fin()) {  //Mientras no llegue al final
+                        listaFinal.agregarFinal(listaNumerosResultanteDeLaRecursion.proximo());
+                    }
+                }
+            }
+            listaFinal.agregarFinal(arbolGeneral.getDato()); //proceso dato, Agrego el dato del arbol a la lista final
+            System.out.println("Se agrego el dato: " + arbolGeneral.getDato());
+        }
+        return listaFinal;
+    }
+
+    public ListaGenerica<Integer> recorrerInOrder(ArbolGeneral<Integer> arbolGeneral) {
+        System.out.println("Entro al arbol con dato (Sin guardarlo, solo entro): " + arbolGeneral.getDato());
+        ListaGenerica<Integer> listaFinal = new ListaEnlazadaGenerica<Integer>();
+        if (!arbolGeneral.esVacio()) {
+            if (arbolGeneral.tieneHijos()) {
+                ListaGenerica<ArbolGeneral<Integer>> listaHijos = arbolGeneral.getHijos();//Guardo los hijos del arbol en otra estructura auxiliar
+                listaHijos.comenzar();                                                     //Preparo la lista
+
+                ArbolGeneral<Integer> hijoActual = listaHijos.proximo();    //Retorna el elemento actual y avanza al siguiente.
+
+                ListaGenerica<Integer> listaNumerosResultanteDeLaRecursion = this.recorrerInOrder(hijoActual);
+                while (!listaNumerosResultanteDeLaRecursion.fin()) {  //Mientras no llegue al final
+                    listaFinal.agregarFinal(listaNumerosResultanteDeLaRecursion.proximo());
+                }
+                listaFinal.agregarFinal(arbolGeneral.getDato()); //proceso dato, Agrego el dato del arbol a la lista final
+                System.out.println("Se agrego el dato ya que se proceso el primer hijo (izq): " + arbolGeneral.getDato());
+
+                while (!listaHijos.fin()) { //Miestras la lista de hijos no llegue a su fin. fin() da true si llegamos al ultimo elemento de la lista
+                    hijoActual = listaHijos.proximo();    //Retorna el elemento actual y avanza al siguiente.
+                    listaNumerosResultanteDeLaRecursion = this.recorrerInOrder(hijoActual);
+                    while (!listaNumerosResultanteDeLaRecursion.fin()) {  //Mientras no llegue al final
+                        listaFinal.agregarFinal(listaNumerosResultanteDeLaRecursion.proximo());
+                    }
+                }
+            } else {
+                listaFinal.agregarFinal(arbolGeneral.getDato()); //proceso dato, Agrego el dato del arbol a la lista final
+                System.out.println("Se agrego el dato ya que no tenia hijos el arbol: " + arbolGeneral.getDato());
+            }
+
         }
         return listaFinal;
     }
@@ -83,12 +117,12 @@ public class RecorridosAG {
     public ListaGenerica<Integer> numerosImparesMayoresQuePreOrden(ArbolGeneral<Integer> a, Integer n) {//podria llamarse diferente, ya que sirve para filtrar
         //Método que retorna una lista con los elementos impares del árbol “a” que sean mayores
         //al valor “n” pasados como parámetros, recorrido en preorden.
-        ListaGenerica<Integer> listaPreOrder = recorrerPreOrder(a);
+        ListaGenerica<Integer> listaRecorrida = recorrerPreOrder(a);
         ListaGenerica<Integer> listaFiltrada = new ListaEnlazadaGenerica<Integer>();
-        while (!listaPreOrder.fin()) {
-            Integer numero = listaPreOrder.proximo();
+        while (!listaRecorrida.fin()) {
+            Integer numero = listaRecorrida.proximo();
             if (esImpar(numero) && esMayor(numero, n)) {
-                System.out.println("Numero........!!.. " + numero);
+//                System.out.println("Numero agregado a la lista filtrada: " + numero);
                 listaFiltrada.agregarFinal(numero);
             }
         }
@@ -104,7 +138,16 @@ public class RecorridosAG {
     public ListaGenerica<Integer> numerosImparesMayoresQuePostOrden(ArbolGeneral<Integer> a, Integer n) {
         //Método que retorna una lista con los elementos impares del árbol “a” que sean mayores
         //    al valor “n” pasados como parámetros recorrido en postorden.
-        return null;
+        ListaGenerica<Integer> listaRecorrida = recorrerPostOrder(a);
+        ListaGenerica<Integer> listaFiltrada = new ListaEnlazadaGenerica<Integer>();
+        while (!listaRecorrida.fin()) {
+            Integer numero = listaRecorrida.proximo();
+            if (esImpar(numero) && esMayor(numero, n)) {
+//                System.out.println("Numero agregado a la lista filtrada: " + numero);
+                listaFiltrada.agregarFinal(numero);
+            }
+        }
+        return listaFiltrada;
     }
 
     public ListaGenerica<Integer> numerosImparesMayoresQuePorNiveles(ArbolGeneral<Integer> a, Integer n) {
